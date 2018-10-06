9b5e8d44398bf409145a4947189a8611f250536f
Valeriy Makarshin <valerymakarshin@gmail.com>
2018-10-07 02:39:51 +0300

    Finished api request for get

--- src/service/api.py
+++ src/service/api.py
@@ -3,17 +3,17 @@ import requests
 from src.utils.setting import get_bitbucket_access_token
 
 ACCESS_TOKEN = 'Bearer {}'.format(get_bitbucket_access_token())
-BASE_PROJECT_URL = 'http://git.moscow.alfaintra.net/projects/AM/repos/am-android/browse'
+BASE_PROJECT_URL = 'http://git.moscow.alfaintra.net/rest/api/latest/projects/AM/repos/am-android/settings/'
 
 
-def get_request():
-    headers = {'Authorization': 'Bearer ' + ACCESS_TOKEN}
+def get_request(url_postfix: str):
+    headers = {'Authorization': ACCESS_TOKEN}
     r = requests.get(
-        'http://git.moscow.alfaintra.net/rest/api/latest/projects/AM/repos/am-android/settings/pull-requests',
+        BASE_PROJECT_URL + url_postfix,
         headers=headers
     )
     print(r.text)
     return r
 
 
-get_request()
+get_request('pull-requests')
