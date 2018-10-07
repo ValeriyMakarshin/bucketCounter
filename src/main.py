ca4b52b7ec55bbe5a9f6c731a525f25fb3372df5
Valeriy Makarshin <valerymakarshin@gmail.com>
2018-10-07 22:02:14 +0300

    Added comment

--- src/main.py
+++ src/main.py
@@ -6,18 +6,21 @@ from src.statistic.comment_statistic import count_comment
 from src.utils.timer import Timer
 
 
+# Сколько тредов комментариев начал каждый человек (использовать аккуратно, запрос делается для каждого pr отдельно)
 def print_statistic_comment():
     comments = get_all_comment_from_pull_request()
     statistic = count_comment(comments)
     print(statistic)
 
 
+# Число пройденных pr для каждого человека
 def print_statistic_pull_request_author():
     pull_requests = get_all_pull_request_with_time_limitation(state_params='MERGED')
     statistic = count_author(pull_requests)
     print(statistic)
 
 
+# Число аппрувов, которые поставил человек
 def print_statistic_approved():
     pull_requests = get_all_pull_request_with_time_limitation(state_params='MERGED')
     statistic = count_approved(pull_requests)
